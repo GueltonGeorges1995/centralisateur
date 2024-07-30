e7073e47ab80935c257bb9553abf3ca2f0195c46
GueltonGorges1995 <georges.guelton@gmail.com>
2024-07-30 10:17:32 +0200

    change navigation. Can go to users now

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,21 +1,23 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    
+
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-              
+
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('items.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
-                
+
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('items.index')" :active="request()->routeIs('items.index')">
                         {{ __('Équipements') }}
                     </x-nav-link>
                 </div>
+
+
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('places.index')" :active="request()->routeIs('places.index')">
                         {{ __('Emplacements') }}
@@ -51,10 +53,12 @@
                         {{ __('Fournisseurs') }}
                     </x-nav-link>
                 </div>
-                
+
+
+
             </div>
 
-         
+
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -73,8 +77,12 @@
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
+                        <x-dropdown-link :href="route('users.index')">
+                            {{ __('Users') }}
+                        </x-dropdown-link>
 
-                
+
+
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -88,7 +96,7 @@
                 </x-dropdown>
             </div>
 
-          
+
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -100,7 +108,7 @@
         </div>
     </div>
 
-    
+
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('items.index')" :active="request()->routeIs('items.index')">
@@ -129,7 +137,7 @@
             </x-responsive-nav-link>
         </div>
 
-      
+
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
@@ -140,8 +148,11 @@
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('users.index')">
+                    {{ __('Users') }}
+                </x-responsive-nav-link>
 
-             
+
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
